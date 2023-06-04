aa68e07b9469aac6a4d8fe86db3c9400220746a6
cip170 <charlottephilipps@gmail.com>
2023-06-04 20:18:45 +1000

    Add JavaScript to project

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,9 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 for (var x = 0; x <topics.length; x++) {
     console.log(topics[x]);
-}
+};
 
-/*
+
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -15,5 +15,4 @@ if (topic === 'HTML') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
-}
-*/
+};
